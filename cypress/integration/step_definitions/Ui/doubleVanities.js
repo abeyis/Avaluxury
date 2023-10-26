fc335a686797d1c820b9de391d25f14cf85d081a
Selma Berg <selma.berg@abeyis.com>
2023-10-26 17:24:46 +0200

    double vanities page clickable

---
+++ cypress/integration/step_definitions/Ui/doubleVanities.js
@@ -0,0 +1,14 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { homePage } from "../../../pages/homePage";
+
+let HomePage=null
+before(()=>{
+  HomePage =new homePage();
+})
+
+When('User clicks to Double Bathroom Vanities', () => {
+    HomePage.clickDoubleVanities();
+});
+
+
+
