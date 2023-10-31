da6dccc308b110171d067f82d484ba5ad949f776
nursena-tekoglu <nursena.tekoglu@abeyis.com>
2023-10-31 20:02:42 +0000

    commented lines are removed

--- cypress/integration/step_definitions/Ui/single_vanities.cy.js
+++ cypress/integration/step_definitions/Ui/single_vanities.cy.js
@@ -6,17 +6,6 @@ import { homePage } from "../../../pages/homePage";
 import { SingeVanitiesPage } from "../../../pages/single_vanities_page";
 
 
-//     Background: User is on the home page
-//         Given User navigates to Home Page
-
-//     Scenario: Clicking the "Single Vanities" button redirects to Single Bathroom Vanities page
-//         When User clicks to Single Vanities
-//         Then User verifies the page title is "Single Bathroom Vanities"
-
-
-
-   
-
 let HomePage=null
 before(()=>{
     HomePage =new homePage();
