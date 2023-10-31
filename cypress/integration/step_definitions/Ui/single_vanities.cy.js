dd43224ac5dc016bf5c365ed99bccdad39ade8f4
nursena-tekoglu <nursena.tekoglu@abeyis.com>
2023-10-31 17:31:53 +0000

    removed the filter test (move them to other branch

--- cypress/integration/step_definitions/Ui/single_vanities.cy.js
+++ cypress/integration/step_definitions/Ui/single_vanities.cy.js
@@ -32,27 +32,6 @@ Then('User verifies the page title is "Single Bathroom Vanities"', (pageTitle) =
 }); 
 
 
-// Scenario: Color filter test (green & black)
-//         When User clicks to Single Vanities
-//         And User clicks the Color Filter Button
-//         And User clicks Green colour
-//         And The page should display only the green vanities 
-//         And User clicks Black Color
-//         Then the page should display green and black vanities
-
-
-When('User clicks to Single Vanities', () => {
-    HomePage.clickSingleVanities();
-  });
-  
-  And ('User clicks the Color Filter Button', () => {
-    SingeVanitiesPage. clickColorButton();
-  });
-  
-  And ('User clicks Green colour', () => {
-    SingeVanitiesPage.clickGreenColour();
-  })
-
 
 
 
