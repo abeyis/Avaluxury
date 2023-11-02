3f60b19c7c354e4a11caa9a0ebaaa78f8356c310
ahmet <ahmet.toprak@abeyis.com>
2023-11-02 20:54:34 +0100

    freestanding

--- cypress/integration/step_definitions/Ui/freestanding.js
+++ cypress/integration/step_definitions/Ui/freestanding.js
@@ -2,25 +2,85 @@ import { Then, When, And } from "cypress-cucumber-preprocessor/steps";
 import { freestandingPage } from "../../../pages/freestandingPage";
 import { homePage } from "../../../pages/homePage";
 
-//let homePage = null
-//let freestandingPage = null
+let HomePage = null
+let FreestandingPage = null
 before(()=>{
-    freestandingPage = new freestandingPage()
-    homePage =new homePage();
+    FreestandingPage = new freestandingPage();
+    HomePage =new homePage();
   })
 When('user goes to freestanding vanities page', () => {
     
-    homePage.clickFreestanding()
+    HomePage.clickFreestanding()
 
 });
 
-And('user picks a color', () => {
+And('user picks {string} as color', (color) => {
 
-freestandingPage.pickColor()
+FreestandingPage.pickColor(color)
 
 });
 
-Then('user should see only color products', () => {
+Then('user should see only {string} products', (color) => {
+FreestandingPage.verifyProductColor(color);
+});
+
+Then('the user should be redirected to the detail page of the product', () => {
+
+});
+
+Given('User navigates to Home Page', () => {
+  FreestandingPage.pickColor()
+});
+
+When('the user clicks on the size', () => {
+});
+
+Then('the user must see {string} all dimensions', (size) => {
+});
+
+Then('the user should be {string} able to select the size', (size) => {
+ 
+});
 
 
+When('the user clicks on the search box', () => {
+});
+
+When('the user chooses {string} the product', (product) => {
+});
+
+Then('the user should only see the selected {string} product', (product) => {
+});
+
+When('the user clicks the add to cart box', () => {
+});
+
+Then('the user should be able to see the products added to the cart', () => {
+});
+
+Then('the user should be able to continue shopping if they want', () => {
+});
+When('the user clicks on the introduction box', () => {
+});
+
+Then('the user should see {string} product numbers', (number) => {
+});
+
+Then('the user must be able to select {string} the number of shows', (number) => {
+});
+
+When('the user sees the details of each product listed with pictures', (number) => {
+});
+When('the user clicks on the {string} box', (number) => {
+
+});
+And('the user should be able to click on the products', (number) => {
+
+});
+And('all the sort variants of the user must be seen', (number) => {
+  
+});
+
+Then('the user should be able to select the desired sort list', (number) => {
+  
 });
--- cypress/pages/freestandingPage.js
+++ cypress/pages/freestandingPage.js
@@ -3,21 +3,117 @@ export class freestandingPage{
 
     freestandingPageSelectors = {
 
-            color: "div[data-filter-id='98948']",
-            colorSearch:".gf-search",
-            colorBeechwood:"button[title='Beechwood']",
-            colorDarkOak:"button[title='Dark Oak']",
-            colorGray:"button[title='Gray']",
-            colorGreen:"button[title='Green']",
-            colorNavyBlue:"button[title='Navy Blue']",
-            colorWhite:"button[title='White']",
-         
+        color: "div[data-filter-id='98948']",
+        colorSearch:".gf-search",
+        colorBeechwood:"button[title='Beechwood']",
+        colorDarkOak:"button[title='Dark Oak']",
+        colorGray:"button[title='Gray']",
+        colorGreen:"button[title='Green']",
+        colorNavyBlue:"button[title='Navy Blue']",
+        colorWhite:"button[title='White']",
 
+        productColor:"div.h4.spf-product-card__title>a",
+
+        size: "div[data-filter-id='98949']",
+        size18: "button[title='18 Inch']",
+        size24: "button[title='24 Inch']",
+        size30: "button[title='30 Inch']",
+        size36: "button[title='36 Inch']",
+        size40: "button[title='40 Inch']",
+        size48: "button[title='48 Inch']",
+        size60: "button[title='60 Inch']",
+        size72: "button[title='72 Inch']",
+
+        price:"div[data-filter-id='98943']",
+        minPriceInput:"#min-98943",
+        maxPriceInput:"#max-98943",
+      
+
+        yourCart:"h1[class='title title--primary']",
+        yourCartContinueShopping:".underlined-link",
+        yourCartQuantity:".cart-item__quantity",
+        yourCartCheckOut:"button[class='cart__checkout-button button']",
+        yourCartPay:".dynamic-checkout__content",
+        yourCartFollowOnShop:"#NewsletterForm--sections--21128380875025__footer"
+        
 
     }
     
-    pickColor(){
+    pickColor(color){
         cy.get(this.freestandingPageSelectors.color).click()
-        cy.get(this.freestandingPageSelectors.colorBeechwood).click()
+        
+
+            switch (color) {
+              case "Beechwood":
+                cy.get(this.freestandingPageSelectors.colorBeechwood).click()
+                break
+              case "Dark Oak":
+                cy.get(this.freestandingPageSelectors.colorDarkOak).click()
+                break
+              case "Gray":
+                cy.get(this.freestandingPageSelectors.colorGray).click()
+                break
+              case "Green":
+                cy.get(this.freestandingPageSelectors.colorGreen).click()
+                break
+              case "Navy Blue":
+                cy.get(this.freestandingPageSelectors.colorNavyBlue).click()
+                break
+              case "White":
+                cy.get(this.freestandingPageSelectors.colorWhite).click()
+                break
+              default:
+                throw new Error("There is no such color");
+            }
+
     }
-}
+
+
+    verifyProductColor(color){
+        cy.get(this.freestandingPageSelectors.productColor).each((item)=>{
+            expect(item).to.contain(color);
+        })
+    }
+
+    pickSize(size){
+        cy.get(this.freestandingPageSelectors.size).click()
+        
+            switch (size) {
+                case "18 Inch":
+                    cy.get(this.freestandingPageSelectors.size18).click()
+                    break
+                case "24 Inch":
+                    cy.get(this.freestandingPageSelectors.size24).click()
+                    break
+                case "30 Inch":
+                    cy.get(this.freestandingPageSelectors.size30).click()
+                    break
+                case "36 Inch":
+                    cy.get(this.freestandingPageSelectors.size36).click()
+                    break
+                case "40 Inch":
+                    cy.get(this.freestandingPageSelectors.size40).click()
+                    break
+                case "48 Inch":
+                    cy.get(this.freestandingPageSelectors.size48).click()
+                    break
+                case "60 Inch":
+                    cy.get(this.freestandingPageSelectors.size60).click()
+                    break
+                case "72 Inch":
+                    cy.get(this.freestandingPageSelectors.size72).click()
+                    break
+                default:
+                    throw new Error("There is no such size");
+            }
+        }
+        definePriceRange(minPrice, maxPrice) {
+       
+            cy.get(this.freestandingPageSelectors.minPriceInput).type(minPrice)
+            cy.get(this.freestandingPageSelectors.maxPriceInput).type(maxPrice)
+            
+        }
+            
+
+        }
+    
