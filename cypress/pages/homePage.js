c3878dfc1883802aa52895e436ed488c3f053157
KubraSayli20 <kubra@propertyfinder.ae>
2023-11-05 03:54:52 +0300

    shop all page scenario is added

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -2,55 +2,61 @@
 
 export class homePage{
     
-    homePageSelectors={
-     home:".header__active-menu-item",
-     shopAll :"a[id='HeaderMenu-shop-all'] span",
-     singleVanities:"a[id='HeaderMenu-single-vanities'] span",
-     doubleVanities:"a[id='HeaderMenu-double-vanities'] span",
-     style:"summary[id='HeaderMenu-style'] span",
-     freeStandingVanities:"#HeaderMenu-style-freestanding-vanities",
-     floatingVanities:"#HeaderMenu-style-floating-vanities",
-     sizes:"summary[id='HeaderMenu-sizes'] span",
-     colors:"#HeaderMenu-colors",
-     designServices:"a[id='HeaderMenu-design-services'] span",
-     blog:"a[id='HeaderMenu-blog'] span",
-     contact:"a[id='HeaderMenu-contact'] span",
-     about:"a[id='HeaderMenu-about'] span"
-    }
-
-   clickDesignServices(){
-    cy.get(this.homePageSelectors.designServices).click();
-   }
-
-
-
-   clickContact(){
-    cy.get(this.homePageSelectors.contact).click();
-   }
-    
-   clickFloatingBathroomVainities(){
-    cy.get(this.homePageSelectors.style).click();
-    cy.get(this.homePageSelectors.floatingVanities).click();
-
-   }
-
-   clickToSizes(){
-    cy.get(this.homePageSelectors.sizes).click();
-   }
-   
-
-   chooseSize(size) {
-    cy.get('#HeaderMenu-sizes-' + size + '-inch').click();
-    cy.wait(1000);
+  homePageSelectors={
+   home:".header__active-menu-item",
+   shopAll :"a[id='HeaderMenu-shop-all'] span",
+   singleVanities:"a[id='HeaderMenu-single-vanities'] span",
+   doubleVanities:"a[id='HeaderMenu-double-vanities'] span",
+   style:"summary[id='HeaderMenu-style'] span",
+   freeStandingVanities:"#HeaderMenu-style-freestanding-vanities",
+   floatingVanities:"#HeaderMenu-style-floating-vanities",
+   sizes:"summary[id='HeaderMenu-sizes'] span",
+   colors:"#HeaderMenu-colors",
+   designServices:"a[id='HeaderMenu-design-services'] span",
+   blog:"a[id='HeaderMenu-blog'] span",
+   contact:"a[id='HeaderMenu-contact'] span",
+   about:"a[id='HeaderMenu-about'] span",
+   shopAll:'a[id="HeaderMenu-shop-all"] span'
   }
 
-   clickSingleVanities(){
-    cy.get(this.homePageSelectors.singleVanities).click()
-   }
-
-  clickDoubleVanities(){
-    cy.get(this.homePageSelectors.doubleVanities).click();
-   }
-    
+ clickDesignServices(){
+  cy.get(this.homePageSelectors.designServices).click();
  }
+
+
+
+ clickContact(){
+  cy.get(this.homePageSelectors.contact).click();
+ }
+  
+ clickFloatingBathroomVainities(){
+  cy.get(this.homePageSelectors.style).click();
+  cy.get(this.homePageSelectors.floatingVanities).click();
+
+ }
+
+ clickToSizes(){
+  cy.get(this.homePageSelectors.sizes).click();
+ }
+ 
+
+ chooseSize(size) {
+  cy.get('#HeaderMenu-sizes-' + size + '-inch').click();
+  cy.wait(1000);
+}
+
+ clickSingleVanities(){
+  cy.get(this.homePageSelectors.singleVanities).click()
+ }
+
+clickDoubleVanities(){
+  cy.get(this.homePageSelectors.doubleVanities).click();
+ }
+
+ clickShopAll(){
+  cy.get(this.homePageSelectors.shopAll).click();
+
+ }
+  
+}
 
---
+++ cypress/pages/shopAllPage.js
@@ -0,0 +1,57 @@
+export class shopAllPage {
+    
+    clickColor() {
+      cy.contains("span", "Color").click();
+    }
+  
+    getColors() {
+      cy.get("ul.gf-option-box button[data-fvalue]").then(
+        ($buttons) => {
+          const colors = [];
+          $buttons.each((i, el) => {
+            const color = Cypress.$(el).attr("data-fvalue");
+            if (color) {
+              if (color.includes("Inch")) {
+                return false; //break the loop if it starts to get inch checkboxes
+              }
+              colors.push(color);
+            }
+          });
+          cy.wrap(colors).as("colors");
+        }
+      );
+    }
+  
+    selectRandomColor() {
+      cy.get("@colors").then((colors) => {
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        const color = colors[randomIndex];
+        cy.wrap(color).as("selectedColor");
+        cy.wait(2000);
+        cy.get(
+          `ul.gf-option-box button[aria-checked="false"][data-fvalue="${color}"]`
+        ).click({ force: true });
+      });
+    }
+   
+    addToCart(){
+      cy.get('span')
+    .filter(':contains("Add to cart")')
+    .then(($elements) => {
+      
+      const randomIndex = Math.floor(Math.random() * $elements.length);
+      const randomElement = $elements.eq(randomIndex);
+      cy.wrap(randomElement).click();
+    }); 
+    }
+  
+    clickOnContinueShopping(){
+  
+      cy.contains('a.underlined-link', 'Continue shopping').click();
+  
+    }
+  
+  
+    
+  }
+  
